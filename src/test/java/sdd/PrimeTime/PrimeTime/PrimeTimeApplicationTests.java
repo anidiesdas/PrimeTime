820dafe40528b5fea3a1c89fad28ab673fb930eb
An Nguyen <ani.kira188@gmail.com>
2025-04-11 10:57:14 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/sdd/PrimeTime/PrimeTime/PrimeTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package sdd.PrimeTime.PrimeTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
